da745352737cc8b62c69df17e61b680cf0fdbcd7
Angus Macrae <angusdmacrae@gmail.com>
2021-02-20 12:03:32 +0000

    Footer content and styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,14 @@
 export default function Footer() {
   return (
     <footer className='footer'>
-      <p></p>
+      <p>
+        <small>
+          &#169; mattwritesart 2021 | website by{' '}
+          <a href='https://angusmacrae.dev' target='_blank' rel='noopener noreferrer' title='Angus Macrae'>
+            Angus Macrae
+          </a>
+        </small>
+      </p>
     </footer>
   );
 }
