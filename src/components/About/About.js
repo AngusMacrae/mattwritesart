dd5929b2cd9c3cac876468e513d2a6b602f4e389
Angus Macrae <angusdmacrae@gmail.com>
2021-02-20 19:57:55 +0000

    Add CTA buttons at bottom of home page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 export default function About() {
   return (
     <section className='about flow'>
@@ -5,6 +7,14 @@ export default function About() {
         <strong>Hi! I'm matt and I write art.</strong>
       </p>
       <p>I form words into images, exploring the power of negative space and the illusion of distance.</p>
+      <div className='about__cta'>
+        <Link to='/commissions' className='btn btn-secondary'>
+          Commissions
+        </Link>
+        <Link to='/browse' className='btn'>
+          Browse & Buy
+        </Link>
+      </div>
     </section>
   );
 }
