d7141d94b2508097925f82a9ec5d144d86e58ed3
Angus Macrae <angusdmacrae@gmail.com>
2021-02-20 17:27:39 +0000

    Add titles, info chips and Details button to gallery images

--- src/components/Gallery/GalleryItem.js
+++ src/components/Gallery/GalleryItem.js
@@ -3,6 +3,12 @@ import placeholder from '../../assets/images/placeholder.webp';
 export default function GalleryItem({ artwork }) {
   return (
     <div className='gallery-item'>
+      <h2>{artwork.name}</h2>
+      <div className='gallery-item__chips'>
+        {artwork.sold ? <span>Sold!</span> : null}
+        {artwork.prints ? <span>Prints available</span> : null}
+        <button>Details</button>
+      </div>
       <img src={`/gallery-images/${artwork.slug}.png`} alt='' />
       {/* <img src={placeholder} alt='' /> */}
     </div>
