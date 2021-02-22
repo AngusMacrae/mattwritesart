6bb26e627f11756ea5d15a3e3e08b88602c6105b
Angus Macrae <angusdmacrae@gmail.com>
2021-02-22 18:40:46 +0000

    Use CSS Grid + JS for Home showcase masonry

--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -1,11 +1,16 @@
 import ShowcaseItem from './ShowcaseItem';
 
 export default function Showcase({ art }) {
+  const gridStyles = {
+    gridAutoRows: 20,
+    gap: 16,
+  };
+
   return (
     <section className='showcase'>
-      <ul className='showcase-list'>
+      <ul className='showcase-list' style={gridStyles}>
         {art.map(artwork => (
-          <ShowcaseItem artwork={artwork} key={artwork.slug} />
+          <ShowcaseItem artwork={artwork} key={artwork.slug} rowHeight={gridStyles.gridAutoRows} rowGap={gridStyles.gap} />
         ))}
       </ul>
     </section>
--- src/components/Showcase/ShowcaseItem.js
+++ src/components/Showcase/ShowcaseItem.js
@@ -1,15 +1,33 @@
+import { useRef, useLayoutEffect } from 'react';
 import { Link } from 'react-router-dom';
 import arrow from '../../assets/icons/right-arrow.svg';
 
-export default function ShowcaseItem({ artwork }) {
+export default function ShowcaseItem({ artwork, rowHeight, rowGap }) {
+  const showcaseItem = useRef();
+  const showcaseItemContent = useRef();
+
+  function fitToGrid() {
+    const rowSpan = Math.ceil((showcaseItemContent.current.getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
+    showcaseItem.current.style.gridRowEnd = `span ${rowSpan}`;
+  }
+
+  useLayoutEffect(() => {
+    window.addEventListener('resize', fitToGrid);
+    return () => {
+      window.removeEventListener('resize', fitToGrid);
+    };
+  }, []);
+
   return (
-    <li className='showcase-item'>
-      <h2>{artwork.name}</h2>
-      <Link to={`/art/${artwork.slug}`} className='showcase-item__link'>
-        View
-        <img src={arrow} alt='' />
-      </Link>
-      <img className='showcase-item__main-img' src={`/art-images/${artwork.slug}.png`} alt='' />
+    <li className='showcase-item' ref={showcaseItem}>
+      <div className='showcase-item-content' ref={showcaseItemContent}>
+        <h2>{artwork.name}</h2>
+        <Link to={`/art/${artwork.slug}`} className='showcase-item__link'>
+          View
+          <img src={arrow} alt='' />
+        </Link>
+        <img className='showcase-item__main-img' src={`/art-images/${artwork.slug}.png`} alt='' onLoad={fitToGrid} />
+      </div>
     </li>
   );
 }
