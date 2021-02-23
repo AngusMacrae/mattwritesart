596753d1f0fa0b47538bd5849348f8f4784c464f
Angus Macrae <angusdmacrae@gmail.com>
2021-02-23 18:50:37 +0000

    Redesign art gallery

--- src/components/ArtGallery/ArtGalleryItem.js
+++ src/components/ArtGallery/ArtGalleryItem.js
@@ -3,15 +3,12 @@ import { Link } from 'react-router-dom';
 export default function ArtGalleryItem({ artwork }) {
   return (
     <li className='art-gallery__item'>
-      <div className='art-gallery__item--img-container'>
-        <img src={`/art-images/${artwork.slug}.png`} alt='' />
-      </div>
-      <div className='art-gallery__item--content'>
-        <h3>{artwork.name}</h3>
-        <p className='art-gallery__item__availability'>{!artwork.original && !artwork.prints ? 'Unavailable' : !artwork.original && artwork.prints ? 'Prints' : artwork.original && artwork.prints ? 'Original & prints' : 'Original'}</p>
-      </div>
-      <Link to={`/art/${artwork.slug}`} className='btn'>
-        View
+      <Link to={`/art/${artwork.slug}`}>
+        <img className='art-gallery__item--img' src={`/art-images/${artwork.slug}.png`} alt='' />
+        <div className='art-gallery__item--content'>
+          <h3>{artwork.name}</h3>
+          <p className='art-gallery__item__availability'>{!artwork.original && !artwork.prints ? 'Unavailable' : !artwork.original && artwork.prints ? 'Prints' : artwork.original && artwork.prints ? 'Original & prints' : 'Original'}</p>
+        </div>
       </Link>
     </li>
   );
