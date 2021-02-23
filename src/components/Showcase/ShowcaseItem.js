675739681aa4154083e58154d682be75a96642a3
Angus Macrae <angusdmacrae@gmail.com>
2021-02-23 18:12:48 +0000

    Use spotlight boolean on artwork object to drive grid span in Showcase

--- src/components/Showcase/ShowcaseItem.js
+++ src/components/Showcase/ShowcaseItem.js
@@ -1,48 +1,29 @@
-import { useRef, useLayoutEffect } from 'react';
 import { Link } from 'react-router-dom';
 import arrow from '../../assets/icons/right-arrow.svg';
 
 export default function ShowcaseItem({ artwork }) {
-  const showcaseItem = useRef();
-  const showcaseItemContent = useRef();
-
-  function fitToGrid() {
-    const { height, width } = showcaseItemContent.current.getBoundingClientRect();
-    if (height > width) {
-      if (Math.random() > 0.8) {
-        showcaseItem.current.style.gridRowEnd = 'span 4';
-        showcaseItem.current.style.gridColumnEnd = 'span 2';
-      } else {
-        showcaseItem.current.style.gridRowEnd = 'span 2';
-        showcaseItem.current.style.gridColumnEnd = 'span 1';
-      }
-    } else {
-      if (Math.random() > 0.8) {
-        showcaseItem.current.style.gridRowEnd = 'span 2';
-        showcaseItem.current.style.gridColumnEnd = 'span 2';
-      } else {
-        showcaseItem.current.style.gridRowEnd = 'span 1';
-        showcaseItem.current.style.gridColumnEnd = 'span 1';
-      }
-    }
+  let gridClasses = '';
+  if (artwork.height > artwork.width) {
+    gridClasses += 'portrait ';
+  } else {
+    gridClasses += 'landscape ';
+  }
+  // if (artwork.height / artwork.width < 1.2 && artwork.height / artwork.width > 0.85) {
+  //   gridClasses += 'square ';
+  // }
+  if (artwork.spotlight) {
+    gridClasses += 'spotlight ';
   }
 
-  useLayoutEffect(() => {
-    window.addEventListener('resize', fitToGrid);
-    return () => {
-      window.removeEventListener('resize', fitToGrid);
-    };
-  }, []);
-
   return (
-    <li className='showcase-item' ref={showcaseItem}>
-      <div className='showcase-item-content' ref={showcaseItemContent}>
+    <li className={`showcase-item ${gridClasses}`}>
+      <div className='showcase-item-content'>
         <h2>{artwork.name}</h2>
         <Link to={`/art/${artwork.slug}`} className='showcase-item__link'>
           View
           <img src={arrow} alt='' />
         </Link>
-        <img className='showcase-item__main-img' src={`/art-images/${artwork.slug}.png`} alt='' onLoad={fitToGrid} />
+        <img className='showcase-item__main-img' src={`/art-images/${artwork.slug}.png`} alt='' />
       </div>
     </li>
   );
