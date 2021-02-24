a738931b6008035960e5597b3ad1cf713e9da65c
Angus Macrae <angusdmacrae@gmail.com>
2021-02-24 21:14:04 +0000

    Revert "Make home page showcase even, with certain images highlighted"
    This reverts commit 0a3fa2db5f5b92024dcef0dd5bf745652caff973.

--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -2,13 +2,18 @@ import ShowcaseItem from './ShowcaseItem';
 import art from '../../data/art.js';
 
 export default function Showcase() {
+  const gridStyles = {
+    gridAutoRows: 10,
+    gap: 32,
+  };
+
   return (
     <section className='showcase'>
-      <ul className='showcase__grid'>
+      <ul className='showcase__grid' style={gridStyles}>
         {art
           .filter(artwork => artwork.showcase)
           .map(artwork => (
-            <ShowcaseItem artwork={artwork} key={artwork.slug} />
+            <ShowcaseItem artwork={artwork} key={artwork.slug} rowHeight={gridStyles.gridAutoRows} rowGap={gridStyles.gap} />
           ))}
       </ul>
     </section>
--- src/components/Showcase/ShowcaseItem.js
+++ src/components/Showcase/ShowcaseItem.js
@@ -1,15 +1,34 @@
+import { useRef, useLayoutEffect } from 'react';
 import { Link } from 'react-router-dom';
 import useImg from '../../hooks/useImg';
 
-export default function ShowcaseItem({ artwork }) {
+export default function ShowcaseItem({ artwork, rowHeight, rowGap }) {
   const { name, slug, spotlight, height, width, description } = artwork;
   const [imgStatus, onLoad, onError] = useImg();
+  const showcaseItem = useRef();
+  const showcaseItemContent = useRef();
+
+  function fitToGrid() {
+    if (spotlight) {
+      // showcaseItem.current.style.gridColumnEnd = 'span 2';
+    }
+    const rowSpan = Math.ceil((showcaseItemContent.current.getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
+    showcaseItem.current.style.gridRowEnd = `span ${rowSpan}`;
+    onLoad();
+  }
+
+  useLayoutEffect(() => {
+    window.addEventListener('resize', fitToGrid);
+    return () => {
+      window.removeEventListener('resize', fitToGrid);
+    };
+  });
 
   return (
-    <li className={`showcase__item shadow ${height > width ? 'portrait' : 'landscape'} ${imgStatus} ${spotlight ? 'spotlight' : ''}`}>
-      <Link to={`/art/${slug}`} className='showcase__item-content'>
+    <li className={`showcase__item shadow ${imgStatus}`} ref={showcaseItem}>
+      <Link to={`/art/${slug}`} className='showcase__item-content' ref={showcaseItemContent}>
         <h2>{name}</h2>
-        <img src={`/art-images/${slug}.webp`} alt={description} height={height * 1000} width={width * 1000} onLoad={onLoad} onError={onError} />
+        <img src={`/art-images/${slug}.webp`} alt={description} height={height * 1000} width={width * 1000} onLoad={fitToGrid} onError={onError} />
       </Link>
     </li>
   );
