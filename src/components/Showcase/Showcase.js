55c16eaaa234f4f2d4c06cfa88c93b44907c2c6d
Angus Macrae <angusdmacrae@gmail.com>
2021-02-25 20:07:38 +0000

    Drive content from csv file and process in art.js

--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -5,9 +5,11 @@ export default function Showcase() {
   return (
     <section className='showcase'>
       <ul className='showcase-list'>
-        {art.map(artwork => (
-          <ShowcaseItem artwork={artwork} key={artwork.slug} />
-        ))}
+        {art
+          .filter(artwork => artwork.showcase)
+          .map(artwork => (
+            <ShowcaseItem artwork={artwork} key={artwork.slug} />
+          ))}
       </ul>
     </section>
   );
--- src/data/art.js
+++ src/data/art.js
@@ -1,5 +1,29 @@
 import artRaw from './artRaw.json';
 
-export const CATEGORIES = new Set(artRaw.map(artwork => artwork.tags).flat());
+const liveArt = artRaw.filter(artwork => artwork.name && artwork.slug && artwork.date);
 
-export default artRaw;
+function processArtData(artwork) {
+  let { showcase, spotlight } = artwork;
+  showcase = !!showcase;
+  spotlight = !!spotlight;
+  const tags = artwork.tags.split(',');
+
+  return {
+    ...artwork,
+    showcase,
+    spotlight,
+    tags,
+  };
+}
+
+const artProcessed = liveArt.map(processArtData);
+
+const allTags = artProcessed
+  .map(artwork => artwork.tags)
+  .flat()
+  .filter(tag => tag !== '');
+
+export const CATEGORIES = new Set(allTags);
+
+export default artProcessed;
+console.log(artProcessed);
