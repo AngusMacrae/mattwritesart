bceac6b6843821a529a89bc0fd5ed9b46b128a61
Angus Macrae <angusdmacrae@gmail.com>
2021-02-28 12:15:28 +0000

    Add underline on hover to footer link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
       <p className='container-wide'>
         <small>
           &#169; mattwritesart 2021 | website by{' '}
-          <a href='https://angusmacrae.dev' target='_blank' rel='noopener noreferrer' title='Angus Macrae'>
+          <a href='https://angusmacrae.dev' target='_blank' rel='noopener noreferrer' title='Angus Macrae' className='link'>
             Angus Macrae
           </a>
         </small>
