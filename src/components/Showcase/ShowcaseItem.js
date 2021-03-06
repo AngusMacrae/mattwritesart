dac136ef73dd9bfa01238a21bc97857a0d671e4b
Angus Macrae <angusdmacrae@gmail.com>
2021-03-06 15:28:28 +0000

    Fade/scale in showcase images on load

--- src/components/Showcase/ShowcaseItem.js
+++ src/components/Showcase/ShowcaseItem.js
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function ShowcaseItem({ artwork }) {
   const { name, slug, spotlight, height, width, description } = artwork;
+  const [loaded, setLoaded] = useState(false);
 
   let gridClasses = '';
   if (height > width) {
@@ -16,11 +18,16 @@ export default function ShowcaseItem({ artwork }) {
     gridClasses += 'spotlight ';
   }
 
+  function handleLoaded(event) {
+    console.log(event.target);
+    setLoaded(true);
+  }
+
   return (
-    <li className={`showcase__item shadow ${gridClasses}`}>
+    <li className={`showcase__item shadow ${gridClasses} ${loaded && 'loaded'}`}>
       <Link to={`/art/${slug}`} className='showcase__item-content'>
         <h2>{name}</h2>
-        <img src={`/art-images/${slug}.webp`} alt={description} />
+        <img src={`/art-images/${slug}.webp`} alt={description} height={height * 1000} width={width * 1000} onLoad={handleLoaded} />
       </Link>
     </li>
   );
