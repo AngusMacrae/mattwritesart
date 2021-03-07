b7c0b458f76efe40b9a9e14e8925ca205f04256d
Angus Macrae <angusdmacrae@gmail.com>
2021-03-07 18:06:44 +0000

    Remove unneeded code from usePageTitle hook

--- src/hooks/usePageTitle.js
+++ src/hooks/usePageTitle.js
@@ -2,10 +2,6 @@ import { useEffect } from 'react';
 
 export default function usePageTitle(newTitle) {
   useEffect(() => {
-    const prevTitle = document.title;
     document.title = newTitle;
-    return () => {
-      document.title = prevTitle;
-    };
   }, []);
 }
