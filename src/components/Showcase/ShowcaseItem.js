b7253c46e324665b3d6d651ae7aab84d569d623e
Angus Macrae <angusdmacrae@gmail.com>
2021-03-08 17:45:11 +0000

    Clean up ShowcaseItem CSS class logic

--- src/components/Showcase/ShowcaseItem.js
+++ src/components/Showcase/ShowcaseItem.js
@@ -5,26 +5,13 @@ export default function ShowcaseItem({ artwork }) {
   const { name, slug, spotlight, height, width, description } = artwork;
   const [loaded, setLoaded] = useState(false);
 
-  let gridClasses = '';
-  if (height > width) {
-    gridClasses += 'portrait ';
-  } else {
-    gridClasses += 'landscape ';
-  }
-  // if (height / width < 1.2 && height / width > 0.85) {
-  //   gridClasses += 'square ';
-  // }
-  if (spotlight) {
-    gridClasses += 'spotlight ';
-  }
-
   function handleLoaded(event) {
     console.log(event.target);
     setLoaded(true);
   }
 
   return (
-    <li className={`showcase__item shadow ${gridClasses} ${loaded && 'loaded'}`}>
+    <li className={`showcase__item shadow ${height > width ? 'portrait' : 'landscape'} ${spotlight ? 'spotlight' : ''} ${loaded ? 'loaded' : ''}`}>
       <Link to={`/art/${slug}`} className='showcase__item-content'>
         <h2>{name}</h2>
         <img src={`/art-images/${slug}.webp`} alt={description} height={height * 1000} width={width * 1000} onLoad={handleLoaded} />
