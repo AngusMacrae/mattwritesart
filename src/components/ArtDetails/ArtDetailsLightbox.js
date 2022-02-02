7dc4c022c5074c0de2a010d897835ef0b27e5a82
Angus Macrae <angus.2.macrae@bt.com>
2022-02-02 16:22:21 +0000

    Refactor keyUp handler logic in lightbox

--- src/components/ArtDetails/ArtDetailsLightbox.js
+++ src/components/ArtDetails/ArtDetailsLightbox.js
@@ -1,27 +1,33 @@
-import { useEffect } from 'react';
-import close from '../../assets/icons/close.svg';
+import { useEffect, useCallback } from "react";
+import close from "../../assets/icons/close.svg";
 
 export default function ArtDetailsLightbox({ imageSrc, closeLightbox }) {
-  useEffect(() => {
-    document.addEventListener('keyup', handleKeyUp);
-    return () => {
-      document.removeEventListener('keyup', handleKeyUp);
-    };
-  }, []);
+  const handleKeyUp = useCallback(
+    (event) => {
+      if (event.key === "Escape") {
+        closeLightbox();
+      }
+    },
+    [closeLightbox]
+  );
 
-  function handleKeyUp(event) {
-    if (event.key === 'Escape') {
-      closeLightbox();
-    }
-  }
+  useEffect(() => {
+    document.addEventListener("keyup", handleKeyUp);
+    return () => {
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [handleKeyUp]);
 
   return (
-    <div className='art-details__lightbox' onClick={closeLightbox}>
-      <div className='art-details__lightbox-content'>
+    <div className="art-details__lightbox" onClick={closeLightbox}>
+      <div className="art-details__lightbox-content">
         {/* eslint-disable-next-line */}
-        <img src={imageSrc} alt='Lines of text and negative space forming an image' />
+        <img
+          src={imageSrc}
+          alt="Lines of text and negative space forming an image"
+        />
         <button autoFocus>
-          Close <img src={close} alt='Close Lightbox' />
+          Close <img src={close} alt="Close Lightbox" />
         </button>
       </div>
     </div>
