3c8bc4b95b96100fe18e1a894550e06080e73922
Angus Macrae <angus.2.macrae@bt.com>
2022-02-02 16:18:58 +0000

    Replace useEffect call with autoFocus attribute

--- src/components/ArtDetails/ArtDetailsLightbox.js
+++ src/components/ArtDetails/ArtDetailsLightbox.js
@@ -1,13 +1,7 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import close from '../../assets/icons/close.svg';
 
 export default function ArtDetailsLightbox({ imageSrc, closeLightbox }) {
-  const closeBtn = useRef();
-
-  useEffect(() => {
-    closeBtn.current.focus();
-  }, []);
-
   useEffect(() => {
     document.addEventListener('keyup', handleKeyUp);
     return () => {
@@ -26,7 +20,7 @@ export default function ArtDetailsLightbox({ imageSrc, closeLightbox }) {
       <div className='art-details__lightbox-content'>
         {/* eslint-disable-next-line */}
         <img src={imageSrc} alt='Lines of text and negative space forming an image' />
-        <button ref={closeBtn}>
+        <button autoFocus>
           Close <img src={close} alt='Close Lightbox' />
         </button>
       </div>
