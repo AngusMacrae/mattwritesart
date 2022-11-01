f9f14750cf18f9e3527434366ac51464c96ee265
Angus Macrae <angus.2.macrae@bt.com>
2022-11-01 11:13:42 +0000

    Refactor Buy page

--- src/pages/buy/Buy.tsx
+++ src/pages/buy/Buy.tsx
@@ -1,50 +1,14 @@
 import Head from "next/head";
-import * as ga from "../../lib/ga";
 import Header from "../../common/components/Header/Header";
 import Footer from "../../common/components/Footer/Footer";
-import Form from "../../common/components/Form/Form";
 import MetaData from "../../common/components/MetaData/MetaData";
-import BuyOption from "./components/BuyOption/BuyOption";
-import useBuyOptions from "./hooks/useBuyOptions";
-import getPrintDimensions from "./utils/getPrintDimensions";
 import art from "../../data/art";
+import BuyForm from "./components/BuyForm/BuyForm";
 
 import styles from "./styles.module.scss";
-import Button from "../../common/components/Button/Button";
-import clsx from "clsx";
 
 export default function Buy({ artwork }) {
-  const {
-    name,
-    slug,
-    original,
-    prints,
-    height,
-    width,
-    medium,
-    description,
-    buyOgCaption,
-    buyPrtCaption,
-  } = artwork;
-
-  const defaultBuyOption = original ? "Original" : "Print";
-  const [buyOption, handleBuyOptionChange] = useBuyOptions(defaultBuyOption);
-
-  const printDimensions = getPrintDimensions(width, height, slug);
-  const originalDimensions = `${width}"x${height}"`;
-
-  const formSubject = `Order - ${slug}`;
-
-  const recordPurchase = () => {
-    ga.event({
-      action: "purchase",
-      params: {
-        piece: name,
-        type: buyOption,
-        price: buyOption === "print" ? prints : original,
-      },
-    });
-  };
+  const { name, slug, description } = artwork;
 
   return (
     <>
@@ -57,60 +21,10 @@ export default function Buy({ artwork }) {
         />
       </Head>
       <Header />
-      <main className="buy">
-        <section className="container-s flow">
+      <main>
+        <section className={styles.buyFormContainer}>
           <h1>{name}</h1>
-          <Form name="buy" subject={formSubject}>
-            <fieldset className={styles["buy__options"]}>
-              {prints ? (
-                <BuyOption
-                  value="print"
-                  caption={buyPrtCaption}
-                  selected={buyOption === "print"}
-                  handleChange={handleBuyOptionChange}
-                  slug={slug}
-                  description={description}
-                  dimensions={printDimensions}
-                  price={prints}
-                />
-              ) : null}
-              {original ? (
-                <BuyOption
-                  value="original"
-                  caption={buyOgCaption}
-                  selected={buyOption === "original"}
-                  handleChange={handleBuyOptionChange}
-                  slug={slug}
-                  description={description}
-                  dimensions={originalDimensions}
-                  medium={medium}
-                  price={original}
-                />
-              ) : null}
-            </fieldset>
-            <div className={clsx(styles.instructions, "flow")}>
-              <p>
-                To purchase, please fill in your details below. I&apos;ll get
-                back to you ASAP with payment details and to arrange shipping.
-              </p>
-              <p>Thanks so much!</p>
-            </div>
-            <input type="text" name="name" placeholder="Your name" required />
-            <input
-              type="email"
-              name="email"
-              placeholder="Your email"
-              required
-            />
-            <textarea
-              name="message"
-              placeholder="Your message (optional)"
-              rows={5}
-            ></textarea>
-            <Button type="submit" onClick={recordPurchase}>
-              Send!
-            </Button>
-          </Form>
+          <BuyForm artwork={artwork} />
         </section>
       </main>
       <Footer />
---
+++ src/pages/buy/components/BuyForm/BuyForm.tsx
@@ -0,0 +1,95 @@
+import clsx from "clsx";
+
+import Form from "../../../../common/components/Form/Form";
+import Button from "../../../../common/components/Button/Button";
+import useBuyOptions from "../../hooks/useBuyOptions";
+import getPrintDimensions from "../../utils/getPrintDimensions";
+import * as ga from "../../../../lib/ga";
+
+import BuyOption from "./components/BuyOption/BuyOption";
+
+import styles from "./styles.module.scss";
+
+export default function BuyForm({ artwork }) {
+  const {
+    name,
+    slug,
+    original,
+    prints,
+    height,
+    width,
+    medium,
+    description,
+    buyOgCaption,
+    buyPrtCaption,
+  } = artwork;
+
+  const defaultBuyOption = original ? "Original" : "Print";
+
+  const printDimensions = getPrintDimensions(width, height, slug);
+  const originalDimensions = `${width}"x${height}"`;
+
+  const formSubject = `Order - ${slug}`;
+
+  const [buyOption, handleBuyOptionChange] = useBuyOptions(defaultBuyOption);
+
+  const recordPurchase = () => {
+    ga.event({
+      action: "purchase",
+      params: {
+        piece: name,
+        type: buyOption,
+        price: buyOption === "print" ? prints : original,
+      },
+    });
+  };
+
+  return (
+    <Form name="buy" subject={formSubject}>
+      <fieldset className={styles["buy__options"]}>
+        {prints ? (
+          <BuyOption
+            value="print"
+            caption={buyPrtCaption}
+            selected={buyOption === "print"}
+            handleChange={handleBuyOptionChange}
+            slug={slug}
+            description={description}
+            dimensions={printDimensions}
+            price={prints}
+          />
+        ) : null}
+        {original ? (
+          <BuyOption
+            value="original"
+            caption={buyOgCaption}
+            selected={buyOption === "original"}
+            handleChange={handleBuyOptionChange}
+            slug={slug}
+            description={description}
+            dimensions={originalDimensions}
+            medium={medium}
+            price={original}
+          />
+        ) : null}
+      </fieldset>
+      <div className={clsx(styles.instructions, "flow")}>
+        <p>
+          To purchase, please fill in your details below. I&apos;ll get back to
+          you ASAP with payment details and to arrange shipping.
+        </p>
+        <p>Thanks so much!</p>
+      </div>
+      <input type="text" name="name" placeholder="Your name" required />
+      <input type="email" name="email" placeholder="Your email" required />
+      <textarea
+        name="message"
+        placeholder="Your message (optional)"
+        rows={5}
+      ></textarea>
+      <Button type="submit" onClick={recordPurchase}>
+        Send!
+      </Button>
+    </Form>
+  );
+}
